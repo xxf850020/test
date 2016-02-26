e30cc815ae0a65ff45db5d32755c3d07cb440c21
qianfeng <qianfeng@yyyyyyy.local>
2016-02-26 14:24:58 +0800

    122222

---
+++ GitDemo/GitDemo/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 谢喧富. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ GitDemo/GitDemo/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  GitDemo
+//
+//  Created by qianfeng on 16/2/26.
+//  Copyright © 2016年 谢喧富. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
